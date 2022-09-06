82c6b10c1a2ccac61f3d40dc43465b7fb769b66d
Liam Naddell <liamnprg@gmail.com>
2022-09-06 13:11:09 -0400

    show off more

--- src/main.c
+++ src/main.c
@@ -4,13 +4,11 @@ extern char hiasm;
 #include "dtb.h"
 #include "memory.h"
 
-extern uint64_t csrread(uint64_t csrno);
-
-
 typedef struct spec {
 	char extensions[26];
 } spec;
 
+//what extensions are supported
 spec get_extensions() {
 	spec s;
 	char *letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -29,6 +27,8 @@ spec get_extensions() {
 	s.extensions[i] = 0;
 	return s;
 }
+
+//the identifier for the current hart (cpu thread)
 uint64_t get_hartid() {
 	uint64_t mhartid;
 	asm volatile("csrrs %0, mhartid, x0" : "=r"(mhartid) : :);
@@ -41,24 +41,27 @@ uint64_t get_mtvec() {
 	return mtvec;
 }
 
+//what function do we call on a trap
 void set_mtvec(void *addr) {
-	//why 4, do research maybe, riscv priviledged says 4 byte boundry, but 2 workd
 	uint64_t mtvec = (((uint64_t) addr) << 0) & ~(0b11) ;
 	asm volatile("csrrw %0, mtvec, %0" : : "r"(mtvec):);
 	return;
 }
 
 typedef struct exception_cause {
+	//exception code
 	unsigned int code;
+	//interrupt or error/exception
 	char interrupt;
 } exception_cause;
 
+//why an exception was caused
 exception_cause get_mcause() {
 	uint64_t mcause;
 	asm volatile("csrrs %0, mcause, x0" : "=r"(mcause) : :);
 	exception_cause ec;
 	ec.interrupt = mcause >> 63;
-	ec.code = mcause & ~(1LL << 62);
+	ec.code = mcause & ~(1LL << 63);
 	return ec;
 }
 
@@ -79,14 +82,21 @@ void m_mode_c_handler() {
 	while (1) {}
 }
 
+//switches the processor from M-mode to S-mode, which is has less privlige
 void call_in_s_mode(void *fn) {
+	disable_pmp();
+	asm volatile("csrrw %0, sepc, %0" : : "r"(fn):);
+	asm volatile("sret" : : :);
+}
+
+//for all 32 megabytes of memory, make it RWX in all processor modes, pmp= physical memory protection
+void disable_pmp() {
+	//32MB of memory starting at 0x80000000
 	uint64_t top = 0x82000000;
 	asm volatile("csrrw %0, pmpaddr0, %0" : : "r"(top):);
 	//                   L  AARWX
 	uint64_t pmp0cfg = 0b10001111;
 	asm volatile ("csrrw %0, pmpcfg0, %0" : : "r"(pmp0cfg));
-	asm volatile("csrrw %0, sepc, %0" : : "r"(fn):);
-	asm volatile("sret" : : :);
 }
 
 void do_ecall() {
@@ -94,8 +104,10 @@ void do_ecall() {
 	asm volatile("ecall" : : :);
 }
 
+//defined in a.s, calls m_mode_c_handler
 extern void m_mode_handler();
 
+//called from a.s after setting up the stack pointer in sp
 void kmain(void *a, void *dtb) {
 	int hartid = get_hartid();
 	if (hartid != 0) {
@@ -110,14 +122,15 @@ void kmain(void *a, void *dtb) {
 	
 	set_mtvec(m_mode_handler);
 
+
+
+
+	fdt_header *hdr = get_header(dtb);
+
+	print_header(hdr);
+	print_structure(hdr);
+
 	call_in_s_mode(do_ecall);
-
-
-
-	//fdt_header *hdr = get_header(dtb);
-
-	//print_header(hdr);
-	//print_structure(hdr);
 
 	/*mem_init(0x80000000,1);
 	char *buf = malloc(50);
